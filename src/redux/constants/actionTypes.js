d59c4da1d79ee7dbc282a870e180a7f66f26a117
afikanzoro <muchemimutahi@gmail.com>
2022-07-29 23:58:30 +0300

    Setup Linters

--- src/redux/constants/actionTypes.js
+++ src/redux/constants/actionTypes.js
@@ -1,6 +1,6 @@
 export const ActionTypes = {
-    GET_COINS: 'GET_COINS',
-    SELECTED_COIN: 'SELECTED_COIN',
-  };
-  
-  export default ActionTypes;
+  GET_COINS: 'GET_COINS',
+  SELECTED_COIN: 'SELECTED_COIN',
+};
+
+export default ActionTypes;
