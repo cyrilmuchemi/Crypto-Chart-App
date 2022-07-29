786b85dfc4c31adc75fa829b08756e515ad170a5
afikanzoro <muchemimutahi@gmail.com>
2022-07-30 00:36:53 +0300

    Fix Linter errors

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,13 @@
-import React from "react";
-import App from "../App";
-import TestRenderer from 'react-test-renderer'; 
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
-import  store from '../redux/reducers/store';
+import App from '../App';
+import store from '../redux/reducers/store';
 
-it('Test if App renders correctly', () => 
-{ const TREE = TestRenderer.create( 
-<Provider store={store}> 
-<App /> 
-</Provider>, ); expect(TREE).toMatchSnapshot(); }); 
+it('Test if App renders correctly', () => {
+  const TREE = TestRenderer.create(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  ); expect(TREE).toMatchSnapshot();
+});
